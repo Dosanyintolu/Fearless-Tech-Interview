c51f37214aeaf3954d82bc1b22d3fed611d0b487
Doyin <Doyin.osanyintolu@gmail.com>
2022-12-24 01:24:37 -0500

    Included dummy data into the demo Lambda_function.py to test API gateway in AWS. Works as expected, need to include library to support validating SSL certificate.

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,26 @@
 import requests
+import json
 
-request = requests.get('https://www.google.com')
+def lambda_handler(event, context):
 
-if request.status_code == 200:
-    print(request.status_code)
-else:
-    print('There seems to be an error connecting to the domain. This is the errror received: ' + request.status_code)
+# Parse the value into the API Gateway and validate the status code
+
+
+# Body of the response
+    apiResponse = {}
+    apiResponse['days_until_expiration'] = '3'
+    apiResponse['is_valid'] = 'True'
+    apiResponse['domain_name'] = 'google.com'
+
+# HTTP Response Object
+    httpResponse = {}
+    httpResponse['statusCode'] = request.status_code
+    httpResponse['body'] = json.dumps(apiResponse)
+    print(httpResponse)
+
+# Return the response object
+    
+    return httpResponse
 
 
 
